8925d28d962a5d4bb2c6b37f6e04433880d4d0cb
Pavel Veretennikov <vermut@kid.lv>
2017-06-11 22:49:54 +0300

    rewrote to softi2c

--- src/Gestures.cpp
+++ src/Gestures.cpp
@@ -1,25 +1,15 @@
 #include <Arduino.h>
 
-#include <Wire.h>
-#include "paj7620.h"
-#define GES_REACTION_TIME		500				// You can adjust the reaction time according to the actual circumstance.
-#define GES_ENTRY_TIME			800				// When you want to recognize the Forward/Backward gestures, your gestures' reaction time must less than GES_ENTRY_TIME(0.8s).
-#define GES_QUIT_TIME			1000
-
-/*
- * Board	I2C / TWI pins
-   Uno, Ethernet	A4 (SDA), A5 (SCL)
-   Mega2560	20 (SDA), 21 (SCL)
-   Leonardo	2 (SDA), 3 (SCL)
-   Due	20 (SDA), 21 (SCL), SDA1, SCL1
- */
+#include "softi2c_paj7620.h"
+#define GES_REACTION_TIME         500                // You can adjust the reaction time according to the actual circumstance.
+#define GES_ENTRY_TIME            800                // When you want to recognize the Forward/Backward gestures, your gestures' reaction time must less than GES_ENTRY_TIME(0.8s).
+#define GES_QUIT_TIME            1000
 
 extern void modbus_setup();
 extern void modbus_loop();
 extern unsigned int holdingRegs[];
 
-void setup()
-{
+void setup() {
 	Serial.begin(115200);
 	modbus_setup();
 
@@ -27,108 +17,89 @@ void setup()
 
 	Serial.println("\nPAJ7620U2 TEST DEMO: Recognize 9 gestures.");
 
-	error = paj7620Init();			// initialize Paj7620 registers
-	if (error)
-	{
+	error = paj7620Init();            // initialize Paj7620 registers
+	if (error) {
 		Serial.print("INIT ERROR,CODE:");
 		Serial.println(error);
-	}
-	else
-	{
+	} else {
 		Serial.println("INIT OK");
 	}
 	Serial.println("Please input your gestures:\n");
 }
 
-void loop() {
-	modbus_loop();
+void do_modbus(unsigned int delay_time = 0) {
+	long start = millis();
+	do {
+		modbus_loop();
+	} while (millis() - start < delay_time);
+}
 
+void gesture_loop() {
 	uint8_t data = 0, data1 = 0, error;
+	error = paj7620ReadReg(0x43, 1, &data);                // Read Bank_0_Reg_0x43/0x44 for gesture result.
 
-	error = paj7620ReadReg(0x43, 1, &data);				// Read Bank_0_Reg_0x43/0x44 for gesture result.
-	if (!error)
-	{
-		switch (data) 									// When different gestures be detected, the variable 'data' will be set to different values by paj7620ReadReg(0x43, 1, &data).
+	if (!error) {
+		switch (data)                                    // When different gestures be detected, the variable 'data' will be set to different values by paj7620ReadReg(0x43, 1, &data).
 		{
 			case GES_RIGHT_FLAG:
-				delay(GES_ENTRY_TIME);
+				do_modbus(GES_ENTRY_TIME);
 				paj7620ReadReg(0x43, 1, &data);
-				if(data == GES_FORWARD_FLAG)
-				{
+				if (data == GES_FORWARD_FLAG) {
 					Serial.println("Forward");
-					delay(GES_QUIT_TIME);
-				}
-				else if(data == GES_BACKWARD_FLAG)
-				{
+					do_modbus(GES_QUIT_TIME);
+				} else if (data == GES_BACKWARD_FLAG) {
 					Serial.println("Backward");
-					delay(GES_QUIT_TIME);
-				}
-				else
-				{
+					do_modbus(GES_QUIT_TIME);
+				} else {
 					Serial.println("Right");
 				}
 				break;
 			case GES_LEFT_FLAG:
-				delay(GES_ENTRY_TIME);
+				do_modbus(GES_ENTRY_TIME);
 				paj7620ReadReg(0x43, 1, &data);
-				if(data == GES_FORWARD_FLAG)
-				{
+				if (data == GES_FORWARD_FLAG) {
 					Serial.println("Forward");
-					delay(GES_QUIT_TIME);
-				}
-				else if(data == GES_BACKWARD_FLAG)
-				{
+					do_modbus(GES_QUIT_TIME);
+				} else if (data == GES_BACKWARD_FLAG) {
 					Serial.println("Backward");
-					delay(GES_QUIT_TIME);
-				}
-				else
-				{
+					do_modbus(GES_QUIT_TIME);
+				} else {
 					Serial.println("Left");
 				}
 				break;
 			case GES_UP_FLAG:
-				delay(GES_ENTRY_TIME);
+				do_modbus(GES_ENTRY_TIME);
 				paj7620ReadReg(0x43, 1, &data);
-				if(data == GES_FORWARD_FLAG)
-				{
+				if (data == GES_FORWARD_FLAG) {
 					Serial.println("Forward");
-					delay(GES_QUIT_TIME);
-				}
-				else if(data == GES_BACKWARD_FLAG)
-				{
+					do_modbus(GES_QUIT_TIME);
+				} else if (data == GES_BACKWARD_FLAG) {
 					Serial.println("Backward");
-					delay(GES_QUIT_TIME);
-				}
-				else
-				{
+					do_modbus(GES_QUIT_TIME);
+				} else {
 					Serial.println("Up");
 				}
 				break;
 			case GES_DOWN_FLAG:
-				delay(GES_ENTRY_TIME);
+				do_modbus(GES_ENTRY_TIME);
 				paj7620ReadReg(0x43, 1, &data);
-				if(data == GES_FORWARD_FLAG)
-				{
+				if (data == GES_FORWARD_FLAG) {
 					Serial.println("Forward");
-					delay(GES_QUIT_TIME);
-				}
-				else if(data == GES_BACKWARD_FLAG)
-				{
+					do_modbus(GES_QUIT_TIME);
+				} else if (data == GES_BACKWARD_FLAG) {
 					Serial.println("Backward");
-					delay(GES_QUIT_TIME);
-				}
-				else
-				{
+					do_modbus(GES_QUIT_TIME);
+				} else {
 					Serial.println("Down");
 				}
 				break;
 			case GES_FORWARD_FLAG:
 				Serial.println("Forward");
-				delay(GES_QUIT_TIME);
+				do_modbus(GES_QUIT_TIME);
 				break;
 			case GES_BACKWARD_FLAG:
 				Serial.println("Backward");
-				delay(GES_QUIT_TIME);
+				do_modbus(GES_QUIT_TIME);
 				break;
 			case GES_CLOCKWISE_FLAG:
 				Serial.println("Clockwise");
@@ -138,11 +109,15 @@ void loop() {
 				break;
 			default:
 				paj7620ReadReg(0x44, 1, &data1);
-				if (data1 == GES_WAVE_FLAG)
-				{
+				if (data1 == GES_WAVE_FLAG) {
 					Serial.println("wave");
 				}
 				break;
 		}
 	}
+}
+
+void loop() {
+	do_modbus();
+	gesture_loop();
 }
